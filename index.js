de4c447a22e4300fe967f93560b7d0e0c8a40c7c
thefontes <thefontes@gmail.com>
2023-07-08 12:37:09 -0400

    express modules upload

--- index.js
+++ index.js
@@ -1,3 +1,44 @@
+const express = require('express')
+const {json} = require("express");
+const app = express();
+const porta = 3000;
 
+app.set('view engine', 'ejs');
+app.get('/', function (requisicao, resposta){
+    resposta.render("menu");
+});
+app.listen(porta, () => {
+    console.log("Servidor rodando na porta: " + porta)
+});
+app.get('/usuario', function (requisicao, resposta){
+    resposta.render("formusuario");
+});
 
-app.get('/json')
+app.get('/formget', function (requisicao, resposta){
+    const usuario = requisicao.query.usuario;
+    const senha = requisicao.query.senha;
+    const pessoa = {usuario: usuario, senha: senha}
+    resposta.render('formgetresposta', { pessoa });
+});
+
+app.get('/livro', function (requisicao, resposta){
+    resposta.render('formlivro');
+});
+
+app.use(express.urlencoded({extended: true}));
+app.post('/formpost', function (requisicao, resposta){
+    const titulo = requisicao.body.titulo;
+    const autor = requisicao.body.autor;
+    console.log(`Recebido titulo e autor: ${titulo} - ${autor}`);
+    resposta.redirect('/livro');
+});
+
+app.get('/json', function (requisicao, resposta){
+    resposta.render('formjson');
+});
+
+app.use(express.json());
+app.post('/json', function (requisao, resposta){
+    console.log(requisao.body);
+    resposta.json(requisao.body);
+});
